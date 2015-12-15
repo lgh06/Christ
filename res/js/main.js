b46d49d78337abb30d0fb07d81844ac457574995
Gehuan Liu <hnnk@qq.com>
2015-12-15 12:33:58 +0800

    图片滑动 初步OK

--- res/js/main.js
+++ res/js/main.js
@@ -1,38 +1,57 @@
 jQuery(function ($) {
 	
-	var startX = 0;
-	var nowX = 0;
-	var endX = 0;
-	var lastMoveX = 0;
-	$(document).on('touchstart','#lgh',function(e){
-		e.preventDefault();
-		startX = e.originalEvent.changedTouches[0].clientX;
-	});
+	function genImages(position){
+		var images = [];
+		for(var i =1;i<=7;i++){
+			images[i] = new Image();
+			images[i].src = "../res/img/face/"+i+"-"+(position == 'top'?'1':'2')+".png";
+		}
+		return images; //images.length = 8
+	}
 	
-	$(document).on('touchmove','#lgh',function($e){
-		var e = $e.originalEvent;
-		var list = e.touches;
-		var changedList = e.changedTouches;
-		
-		nowX = changedList[0].clientX;
-		
-		console.log(nowX);
-		
-		$('#lgh').css({
-			"transform":'translateX('+(nowX - startX + lastMoveX)+'px)',
-			"-webkit-transform":'translateX('+(nowX - startX + lastMoveX)+'px)'
+	function appendImage(selector,images){
+		for(var i = 1;i<=images.length-1;i++){  //数组下标1到(8-1)
+			$(selector).append(images[i]);
+		}
+	}
+	
+	function slice(selector){
+		var startX = 0;
+		var nowX = 0;
+		var endX = 0;
+		var lastMoveX = 0;
+		$(document).on('touchstart',selector,function(e){
+			e.preventDefault();
+			startX = e.originalEvent.changedTouches[0].clientX;
 		});
 		
+		$(document).on('touchmove',selector,function($e){
+			var e = $e.originalEvent;
+			var list = e.touches;
+			var changedList = e.changedTouches;
+			
+			nowX = changedList[0].clientX;
+			
+			console.log(nowX);
+			
+			$(selector).css({
+				"transform":'translateX('+(nowX - startX + lastMoveX)+'px)',
+				"-webkit-transform":'translateX('+(nowX - startX + lastMoveX)+'px)'
+			});
+			//$('body').html(changedList[0].clientY);
+		});
 		
-		//$('body').html(changedList[0].clientY);
-	});
+		$(document).on('touchend',selector,function(e){
+			endX = e.originalEvent.changedTouches[0].clientX;
+			lastMoveX = endX - startX + lastMoveX;
+		});	
+	}
 	
-	$(document).on('touchend','#lgh',function(e){
-		endX = e.originalEvent.changedTouches[0].clientX;
-		lastMoveX = endX - startX + lastMoveX;
-	});	
+		
+	appendImage('.top',genImages('top'));
+	appendImage('.btm',genImages('btm'));
 	
-	
-	
+	slice('.top');
+	slice('.btm');
 	
 });
